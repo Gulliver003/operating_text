4c556750d6ed91663aa2522607b0b8b927e13a8f
Gulliver003 <73177799+Gulliver003@users.noreply.github.com>
2020-11-17 19:53:57 +0900

    Update csv_to_json.py

--- csv_to_json.py
+++ csv_to_json.py
@@ -1,15 +1,37 @@
+#-*- coding: utf-8 -*-
 import json
 import csv
+from collections import OrderedDict
 
-with open('csvファイルのパス','r') as c:
+with open('/Users/sugiuratakuya/st.csv','r') as c:
     reader = csv.reader(c)
     for index,row in enumerate(reader):
         #書き込みたいjsonの内容
-        data = {
-            'No': row[0],
-            'Name': row[1],
-            'Job': row[2]
+        od = OrderedDict([
+            ('No',row[0]),
+            ('Name',row[1]),
+            ('Job','Manager'),
+            ('A', OrderedDict([('i', row[2]), ('j', 2)])),
+            ('B', OrderedDict([('k', 1), ('l', 2)])),
+            ('Text', 'Hello world'),
+        ])
+        '''
+        以下のように書き込まれる
+        {
+            "No": "1",
+            "Name": "takuya",
+            "Job": "Manager",
+            "A": {
+                "i": "programmer",
+                "j": 2
+            },
+            "B": {
+                "k": 1,
+                "l": 2
+            },
+            "Text": "Hello world"
         }
-        with open('作成したいjsonファイルのパス/test{}.json'.format(index+1), 'w') as f:
+        '''
+        with open('/Users/sugiuratakuya/testjson/test{}.json'.format(index+1), 'w') as f:
             #json形式にエンコード
-            json.dump(data,f,indent=4)
+            json.dump(od,f,indent=4)
